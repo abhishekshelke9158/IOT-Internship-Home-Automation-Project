93cd60c953a8bb96454067445b4740e1a19ad37e
Abhishek Shelke <87358500+abhishekshelke9158@users.noreply.github.com>
2023-09-18 08:56:52 +0530

    serial_tank.h

--- serial_tank.h
+++ serial_tank.h
@@ -1,16 +1,12 @@
 #ifndef SERIAL_TANK_H
 #define SERIAL_TANK_H
 
-
-//input digital
 #define INLET_VALVE  0x00
 #define OUTLET_VALVE 0x01
 
-//sensors digital
 #define HIGH_FLOAT 0x10
 #define LOW_FLOAT  0x11
 
-//sensor analog
 #define VOLUME 0x30
 
 #define   ENABLE  0x01
